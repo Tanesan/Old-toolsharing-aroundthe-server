0e1e34b95b43d6b47eb8d02fe62a96c4ba2dbce3
Tanesan <54165015+Tanesan@users.noreply.github.com>
2020-06-29 21:39:51 +0900

    master

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,5 +1,6 @@
+from flask import request,redirect,url_for,render_template,flash,session
 from flask_blog import app
 
 @app.route('/')
 def show_entries():
-    return "HelloWorld!"
+    return render_template('entries/index.html')
