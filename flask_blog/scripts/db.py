3942654c85d2ec68b433f97a490af07a511bc326
Tanesan <54165015+Tanesan@users.noreply.github.com>
2020-07-01 22:43:20 +0900

    master

--- flask_blog/scripts/db.py
+++ flask_blog/scripts/db.py
@@ -1,7 +1,9 @@
 from flask_script import Command
 from flask_blog import db
+
+
 class InitDB(Command):
-    "Create database"
+    "create database"
 
     def run(self):
         db.create_all()
--- flask_blog/views/views.py
+++ flask_blog/views/views.py
@@ -1,6 +1,7 @@
-from flask import request,redirect,url_for,render_template,flash,session
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
- 
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -18,6 +19,6 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in',None)
+    session.pop('logged_in', None)
     flash('ログアウトしました')
     return redirect(url_for('show_entries'))
