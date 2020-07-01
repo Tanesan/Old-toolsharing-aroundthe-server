e4615dbfde85b6e20ab7a3d6cebe827c43734464
Tanesan <54165015+Tanesan@users.noreply.github.com>
2020-07-01 22:13:07 +0900

    master

--- flask_blog/models/entries.py
+++ flask_blog/models/entries.py
@@ -5,20 +5,13 @@ class Entry(db.Model):
     __tablename__ = 'entries'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), unique=True)
-    text = db.Column(db.text)
+    text = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
 
-    def __init__(self,title=None, text=None):
+    def __init__(self, title=None, text=None):
         self.title = title
         self.text = text
         self.created_at = datetime.utcnow()
 
     def __repr__(self):
         return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
-
-@app.route('/entries/new',methods=['GET'])
-def new_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/new.html')
-    
