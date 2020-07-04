e21e7c152b266c151e4629003f38af9dabe66586
Tanesan <54165015+Tanesan@users.noreply.github.com>
2020-07-04 18:12:11 +0900

    master

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,6 +1,6 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
-SQLALCHEMY_DATABASE_URI='sqlite:///flask_blog.db'
-SQLALCHEMY_TRACK_MODIFICATIONS=True
 SECRET_KEY = 'secret key'
-USERNAME = 'Kei'
-PASSWORD = 'a'
+USERNAME = 'john'
+PASSWORD = 'due123'
--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
-from flask_blog import app
 from flask_script import Manager
+from flask_blog import create_app
+
 from flask_blog.scripts.db import InitDB
-if __name__=='__main__':
-    manager = Manager(app)
-    manager.add_command('init_db',InitDB())
+
+
+if __name__ == "__main__":
+    manager = Manager(create_app)
+    manager.add_command('init_db', InitDB())
     manager.run()
-    
